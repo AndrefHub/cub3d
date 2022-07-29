d10c738a82830b1b93e72a52b91d7a607e978bda
Lion Sherry <lsherry@va-d1.kzn.21-school.ru>
2022-07-29 16:28:33 +0300

    added CMake
    Former-commit-id: af16e9d947ffca543ed063c93bb16bc8cdb55b1a

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@ int	init_game(void);
 
 int main(int ac, char **av)
 {
-	t_map	*map;
-
-	map = parse_file(ac, av);
-	if (map)
-	{
-		print_map_debug(map);
-	}
+//	t_map	*map;
+//
+//	map = parse_file(ac, av);
+//	if (map)
+//	{
+//		print_map_debug(map);
+//	}
 	(void ) ac;
 	(void ) av;
 	init_game();
