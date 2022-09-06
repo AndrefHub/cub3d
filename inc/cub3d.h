f5a8094eb9f0400c9384297e683001af7f620f70
Kristina Dancy <kdancy@va-i5.kzn.21-school.ru>
2022-09-06 16:32:48 +0300

    fixed get_textures
    Former-commit-id: 7b84dcf447924ffe61794901554a616e1df8399c

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -152,6 +152,7 @@ int		ft_strrchr_int(char *line, int chr);
 t_map	*create_empty_map(void);
 int		is_wall(char c);
 int		ft_arraylen(void **arr);
+int		get_string_index(char *str, char c);
 void	get_textures_list(t_map* map, int fd, char **line);
 // char	*get_textures_list(int fd, char *prefix, t_list **lst);
 //controller.c
