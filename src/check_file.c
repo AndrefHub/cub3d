0891771a23d06fdcdc7be8cd3085a50f4c318099
Lion Sherry <lsherry@va-j5.kzn.21-school.ru>
2022-09-06 13:36:58 +0300

    fixed check_file.c
    Former-commit-id: 3112825ca1aaf287a259d64f47c067642500858f

--- src/check_file.c
+++ src/check_file.c
@@ -14,18 +14,20 @@ static int file_exists(char *filename)
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	return (1);
+	return (fd);
 }
 
 int	check_file(int ac, char **av)
 {
 	int fd;
 
+	fd = -1;
 	if (ac != 2)
 		return (BAD_FILE);
 	if (filename_check(av[1]) == 0)
 		return (BAD_FILE);
-	if (file_exists(av[1]) == 0)
+	fd = file_exists(av[1]);
+	if (fd == 0)
 		return (BAD_FILE);
 	return (fd);
 }
