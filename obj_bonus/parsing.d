26dd97843944c3c62e22aeea101a8eac984215de
LudwigAndreas <landreas@MacBook-Pro-2.local>
2022-09-26 13:29:16 +0300

    merged and decreased MAX_RENDER_DISTANCE
    Former-commit-id: f0322bcf45cb4138fbbe4deeb6bfb853d418118d

--- obj_bonus/parsing.d
+++
@@ -1,6 +0,0 @@
-obj_bonus/parsing.o: src_bonus/parsing.c libft/libft.h \
-  inc_bonus/cub3d_bonus.h inc_bonus/../libft/libft.h \
-  inc_bonus/constants_bonus.h /usr/local/include/X11/X.h \
-  /usr/local/include/X11/keysym.h /usr/local/include/X11/keysymdef.h \
-  inc_bonus/../cute_sound/cute_sound.h inc_bonus/../mlx/mlx.h \
-  src_bonus/../inc_bonus/cub3d_bonus.h
