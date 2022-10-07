193f3e8650a61970b90196f0346ae488e3a010aa
Lion Sherry <lsherry@ev-a5.kzn.21-school.ru>
2022-10-07 13:35:43 +0300

    moved hud text
    Former-commit-id: ecd23e4fd71185d3f3c6ba2887b7fc118cf5b38b

--- src_bonus/draw_hud.c
+++ src_bonus/draw_hud.c
@@ -12,11 +12,11 @@ void	draw_hud_entry(t_game *game, t_hud_entry *entry, int offset, int y)
 	ft_put_itoa(output, entry->value);
 	put_text_to_screen_layout(game, &(t_text){
 		entry->title, (t_vector)
-		{text_pos.x,text_pos.y}, VTop | HLeft}, game->hud.font_size); //TODO: put title and value on different strings
+		{text_pos.x,text_pos.y}, VTop | HCenter}, game->hud.font_size); //TODO: put title and value on different strings
 	put_text_to_screen_layout(game, &(t_text){
 			output, (t_vector)
 					{text_pos.x,
-					 text_pos.y + game->hud.font_size * 1.5f}, VTop | HLeft}, game->hud.font_size);
+					 text_pos.y + game->hud.font_size * 1.5f}, VTop | HCenter}, game->hud.font_size);
 }
 
 void	draw_hud(t_game *game)
@@ -24,22 +24,9 @@ void	draw_hud(t_game *game)
 	int	y;
 
 	y = 2;
-//	fill_img_color(&game->hud_img, 0x000000);
 	put_image_to_image(&game->hud_img, (t_vector) {0, 0}, &game->hud_texture);
 	draw_hud_entry(game, &game->hud.fps, game->hud.fps.title_size, ++y * 3);
 	draw_hud_entry(game, &game->hud.score, game->hud.score.title_size, ++y * 3);
 	draw_hud_entry(game, &game->hud.lives, game->hud.lives.title_size, ++y * 3);
 	draw_hud_entry(game, &game->hud.health, game->hud.health.title_size, ++y * 3);
-//	put_text_to_screen_layout(game, &(t_text){
-//		"a", (t_vector){game->mlx.win_size.x - (game->mlx.win_size.x - game->img.size.x) / 2, 30 * ++y}, VTop | HLeft
-//		}, 120
-//	);
-	// mlx_string_put(game->mlx.id, game->mlx.window, 40, 15, 0x00FFFFFF,
-	// 			   "tr:");
-	// mlx_string_put(game->mlx.id, game->mlx.window, 60, 15, 0x00FFFFFF, 
-	// 	(char []){'0' + time1 / 1000, '0' + time1 / 100 % 10, '0' + time1 / 10 % 10, '0' + time1 % 10, '\0'});
-	// mlx_string_put(game->mlx.id, game->mlx.window, 100, 15, 0x00FFFFFF,
-	// 			   "td:");
-	// mlx_string_put(game->mlx.id, game->mlx.window, 120, 15, 0x00FFFFFF, 
-	// 	(char []){'0' + time2 / 1000, '0' + time2 / 100 % 10, '0' + time2 / 10 % 10, '0' + time2 % 10, '\0'});
 }
--- src_bonus/start_game.c
+++ src_bonus/start_game.c
@@ -41,8 +41,8 @@ static inline void	get_screen_size(void *mlx_ptr, int *size_x, int *size_y)
 {
 	(void)mlx_ptr;
 	mlx_get_screen_size(size_x, size_y);
-	*size_x = 1280;
-	*size_y = 720;
+//	*size_x = 1280;
+//	*size_y = 720;
 }
 
 #else
