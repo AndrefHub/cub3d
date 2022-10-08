622acb8cb2df1495f1bbc85128343c37c0054e40
Lion Sherry <lsherry@ev-a5.kzn.21-school.ru>
2022-10-08 16:26:28 +0300

    changed cos and sin to player.vector
    Former-commit-id: e590703a746b646f926801a9061fd15cb7082863

--- obj_bonus/player_eating.d
+++
@@ -1,4 +0,0 @@
-obj_bonus/player_eating.o: src_bonus/player_eating.c libft/libft.h \
- inc_bonus/cub3d_bonus.h inc_bonus/../libft/libft.h \
- inc_bonus/constants_bonus.h inc_bonus/../cute_sound/cute_sound.h \
- inc_bonus/../mlx/mlx.h src_bonus/../inc_bonus/cub3d_bonus.h
