da62212f968f4469a31bceca56964cd3afd43807
Lion Sherry <lsherry@ev-o3.kzn.21-school.ru>
2022-11-05 18:11:50 +0300

    norminetted raycasting.c
    Former-commit-id: bf19f2e585b59b1e28e62341c65d08bcc4f2ea99

--- src_bonus/get_screen_size.c
+++ src_bonus/get_screen_size.c
@@ -5,9 +5,7 @@
 inline void	get_screen_size(void *mlx_ptr, int *size_x, int *size_y)
 {
 	(void)mlx_ptr;
-//	mlx_get_screen_size(size_x, size_y);
-	*size_x = 1920;
-	*size_y = 1080;
+	mlx_get_screen_size(size_x, size_y);
 }
 
 #else
